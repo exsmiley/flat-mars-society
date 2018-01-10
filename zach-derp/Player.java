76c34950502ff639732f61946a45ce425b85fdc8
Zachery Miranda <exsmiley@gmail.com>
2018-01-10 00:16:03 -0500

    robot chaoosssss

--- zach-derp/Player.java
+++ zach-derp/Player.java
@@ -3,6 +3,37 @@
 import bc.*;
 
 public class Player {
+	
+	public static void moveRobotSpiral(GameController gc, int id) {
+		if(gc.canMove(id, Direction.North)) {
+			gc.moveRobot(id, Direction.North);
+		}
+		else if(gc.canMove(id, Direction.West)) {
+			gc.moveRobot(id, Direction.West);
+		}
+		else if(gc.canMove(id, Direction.South)) {
+			gc.moveRobot(id, Direction.South);
+		}
+		else if(gc.canMove(id, Direction.East)) {
+			gc.moveRobot(id, Direction.East);
+		}
+	}
+	
+	public static void replicateSomewhere(GameController gc, int id) {
+		if(gc.canReplicate(id, Direction.South)) {
+			gc.replicate(id, Direction.South);
+		}
+		else if(gc.canReplicate(id, Direction.East)) {
+			gc.replicate(id, Direction.East);
+		}
+		else if(gc.canReplicate(id, Direction.North)) {
+			gc.replicate(id, Direction.North);
+		}
+		else if(gc.canReplicate(id, Direction.West)) {
+			gc.replicate(id, Direction.West);
+		}
+	}
+	
     public static void main(String[] args) {
         // MapLocation is a data structure you'll use a lot.
         MapLocation loc = new MapLocation(Planet.Earth, 10, 20);
@@ -23,20 +54,18 @@ public class Player {
             System.out.println("Current round: "+gc.round());
             // VecUnit is a class that you can think of as similar to ArrayList<Unit>, but immutable.
             VecUnit units = gc.myUnits();
+            
+            System.out.println("I have " + units.size() + " units! :)");
 
             for (int i = 0; i < units.size(); i++) {
                 Unit unit = units.get(i);
+                int id = unit.id();
 
-                // Most methods on gc take unit IDs, instead of the unit objects themselves.
-                if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), Direction.North)) {
-                    gc.moveRobot(unit.id(), Direction.North);
-                    if(gc.canReplicate(unit.id(), Direction.South))
-                    gc.replicate(unit.id(), Direction.South);
-                    
-                    if(gc.canBlueprint(unit.id(), UnitType.Factory, Direction.West)) {
-                    		gc.blueprint(unit.id(), UnitType.Factory, Direction.West);
-                    }
+                if(gc.isMoveReady(id)) {
+                		moveRobotSpiral(gc, id);
+                		replicateSomewhere(gc, id);
                 }
+                
             }
             // Submit the actions we've done, and wait for our next turn.
             gc.nextTurn();
