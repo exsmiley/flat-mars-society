05e14261f26c75a959111b937412a35e2f87f3a3
Darius Bopp <dbopp@mit.edu>
2018-01-10 19:59:48 -0500

    Darius player

--- DariusPlayer/Player.java
+++ DariusPlayer/Player.java
@@ -1,6 +1,7 @@
 // import the API.
 // See xxx for the javadocs.
 import bc.*;
+import math;
 
 public class Player {
     public static void main(String[] args) {
@@ -18,6 +19,8 @@ public class Player {
 
         // Direction is a normal java enum.
         Direction[] directions = Direction.values();
+        
+        Team my_team = gc.team();
 
         while (true) {
             System.out.println("Current round: "+gc.round());
@@ -25,11 +28,46 @@ public class Player {
             VecUnit units = gc.myUnits();
             for (int i = 0; i < units.size(); i++) {
                 Unit unit = units.get(i);
-
-                // Most methods on gc take unit IDs, instead of the unit objects themselves.
-                if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), Direction.Southeast)) {
-                    gc.moveRobot(unit.id(), Direction.Southeast);
+                
+                if unit.unitType == bc.UnitType.Factory {
+                	if gc.canProduceRobot(unit.id(), bc.UnitType.Knight){
+                		gc.produceRobot(unit.id(), bc.UnitType.Knight);
+                		System.out.println("Made a Knight");
+                	}
                 }
+                
+                Location location = unit.location();
+                if location.is_on_map(){
+                	VecUnit nearby = gc.senseNearbyUnits(unit.map_location(),2)
+                	for (int i = 0; i < nearby.size(); i++) {
+                		Unit other = nearby.get(i);
+                		if gc.canBuild(unit.id(), other.id()){
+                			gc.build(unit.id(), other.id());
+                			System.out.println("Built a thing");
+                			continue;
+                		}
+                		if ((other.team() != my_team) && gc.isAttackReady(unit.id()) && gc.can_attack(unit.id(), other.id()){
+                			gc.attack(unit.id(), other.id());
+                			System.out.println("Attached a thing")
+                			continue;
+                		
+                	}
+                }
+                
+                //try and make a factory
+                if (gc.karbonite() >= bc.unitType.Factory.blueprintCost() && gc.canBlueprint(unit.id(), bc.UnitType.Facory, Direction.North)) {
+                	gc.blueprint(unit.id(), bc.unitType.Facorty, Direction.North);
+                } else if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), Direction.South){
+                	gc.moveRobot(unit.id(),Direction.South);
+                }
+                	
+                
+                	
+                // Most methods on gc take unit IDs, instead of the unit objects themselves.
+                //if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), Direction.Southeast)) {
+                //    gc.moveRobot(unit.id(), Direction.Southeast);
+                //}
+                
             }
             // Submit the actions we've done, and wait for our next turn.
             gc.nextTurn();
