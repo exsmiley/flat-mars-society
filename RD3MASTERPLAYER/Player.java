7ff1c8f441d81742ff0552d4347cc76c61ab3b82
Ron Dentinger <rldentin@mit.edu>
2018-01-10 19:49:41 -0500

    Converted the python to my player.

--- RD3MASTERPLAYER/Player.java
+++ RD3MASTERPLAYER/Player.java
@@ -18,28 +18,71 @@ public class Player {
 
         // Direction is a normal java enum.
         Direction[] directions = Direction.values();
+        
+        Team myTeam = gc.team();
 
         while (true) {
-            System.out.println("Current round: "+gc.round());
-            // VecUnit is a class that you can think of as similar to ArrayList<Unit>, but immutable.
-            VecUnit units = gc.myUnits();
-            for (int i = 0; i < units.size(); i++) {
-                if (gc.planet().equals(Planet.Earth)) {
+            try {
+                System.out.println("Current round: "+gc.round());
+                // VecUnit is a class that you can think of as similar to ArrayList<Unit>, but immutable.
+                VecUnit units = gc.myUnits();
+                for (int i = 0; i < units.size(); i++) {
                     Unit unit = units.get(i);
-                    if (gc.canBlueprint(unit.id(), UnitType.Factory, Direction.South)) {
-                        System.out.println("Trying to blueprint....");
-                        gc.blueprint(unit.id(), UnitType.Factory, Direction.South);
-                        System.out.println("Blueprint succeded");
+                    
+                    // First factory logic
+                    if (unit.unitType().equals(UnitType.Factory)) {
+                        VecUnitID garrison = unit.structureGarrison();
+                        if (garrison.size() > 0) {
+                            Direction d = directions[(int)Math.random()*4];
+                            if (gc.canUnload(unit.id(), d)) {
+                                System.out.println("Unloaded a Knight!");
+                                gc.unload(unit.id(), d);
+                                continue;
+                            }
+                        }
+                        else if (gc.canProduceRobot(unit.id(), UnitType.Knight)) {
+                            gc.produceRobot(unit.id(), UnitType.Knight);
+                            System.out.println("Produced a Knight!");
+                            continue;
+                        }
                     }
-    
-                    // Most methods on gc take unit IDs, instead of the unit objects themselves.
-                    else if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), Direction.East)) {
-                        gc.moveRobot(unit.id(), Direction.East);
+                    
+                    // First, look for nearby blueprints to work on.
+                    Location location = unit.location();
+                    if (location.isOnMap()) {
+                        VecUnit nearby = gc.senseNearbyUnits(location.mapLocation(), 2);
+                        for (int j = 0; j < nearby.size(); j++) {
+                            Unit other = nearby.get(j);
+                            if (unit.unitType().equals(UnitType.Worker) && (gc.canBuild(unit.id(), other.id()))) {
+                                gc.build(unit.id(), other.id());
+                                System.out.println("Built a factory!");
+                                // move onto the next unit;
+                                continue;
+                            }
+                            if (!(other.team().equals(myTeam)) && (gc.isAttackReady(unit.id())) && gc.canAttack(unit.id(), other.id())) {
+                                System.out.println("Attacked a thing.");
+                                gc.attack(unit.id(), other.id());
+                                continue;
+                            }
+                        }
                     }
+                    
+                    // Ok there weren't any dudes around, pick a direction at random
+                    Direction d = directions[(int)Math.random()*4];
+                    
+                    // or, try to build a factory:
+                    if ((gc.karbonite() > 100) && (gc.canBlueprint(unit.id(), UnitType.Factory, d))) {
+                        gc.blueprint(unit.id(), UnitType.Factory, d);
+                    }
+                    
+                    // If that fails try to move.
+                    else if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), d)) {
+                        gc.moveRobot(unit.id(), d);
+                    }
+                    
                 }
-                else {
-                    System.out.println("WE ON MARS.");
-                }
+            } catch (Exception e) {
+                System.out.println("ERRRORRORORROOROR");
             }
             // Submit the actions we've done, and wait for our next turn.
             gc.nextTurn();
